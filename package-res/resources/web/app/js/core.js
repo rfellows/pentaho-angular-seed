7ac6bb9b9019f138ce776145f9049cdf22e1c11b
Rob Fellows <rfellows@pentaho.com>
2013-12-11 12:05:19 -0500

    Adding core.js dummy module

---
+++ package-res/resources/web/app/js/core.js
@@ -0,0 +1,5 @@
+define([], function(){
+  // dummy module for building application and making it available for use
+  // _sample_/app gets inserted here by way of the module definition in
+  // javascript.build.js
+});
